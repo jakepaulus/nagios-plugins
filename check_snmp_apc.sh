49beed40e08819f05dd9e222f8938ce7e080e8c4
Jake Paulus <jakepaulus@users.noreply.github.com>
2016-03-22 13:33:19 -0400

    Update check_snmp_apc.sh
    Made output more terse, added temperature check, and added perfdata support

--- check_snmp_apc.sh
+++ check_snmp_apc.sh
@@ -3,7 +3,7 @@
 # This script checks the status of APC battery backups using snmp data provided
 # by the APC network management card.
 #
-# Last modified 20090514 by Jake Paulus
+# Last modified 20160322 by Jake Paulus
 
 print_usage() {
         echo ""
@@ -16,9 +16,12 @@ print_usage() {
         echo "generalstatus"
         echo "batterystatus"
         echo "load"
+        echo "temperature \$warning \$critical"
         echo ""
-        echo "Please be aware that the three tests above do not overlap in functionality"
+        echo "Please be aware that the tests above do not overlap in functionality"
         echo "by very much so it's worth running them all."
+        echo ""
+        echo "The temperature test is in degrees Fahrenheit"
         echo ""
         exit 3
 }
@@ -42,13 +45,13 @@ if [ $3 = 'generalstatus' ] ; then
         #APC UPS :: Basic Output Status (On-line)
         mytest=`echo $mytest | awk '{print $NF}'`
         if [[ $mytest -eq 2 ]] ; then
-                echo "OK - ${statuses[$mytest]} Please see http://$2/ for more information."
+                echo "OK"
                 exit 0
         elif [[ $mytest -eq 1 ]] ; then
-                echo "Unknown - Status unknown Please see http://$2/ for more information."
+                echo "Unknown - Status unknown. See http://$2/"
                 exit 3
         else
-                echo "Critical - ${statuses[$mytest]} Please see http://$2/ for more information."
+                echo "Critical - ${statuses[$mytest]} See http://$2/"
                 exit 2
         fi
 elif [ $3 = 'batterystatus' ] ; then
@@ -58,45 +61,40 @@ elif [ $3 = 'batterystatus' ] ; then
 
         #APC UPS :: Advanced Battery Capacity
         mytest=`snmpget -r 3 -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.1.2.2.1.0 | awk '{print $NF}'`
-        if [[ $mytest -gt 85 ]] ; then
-                notices="$mytest% capacity remaining "
-        else
-                notices="$mytest% capacity remaining which is less than 85% "
+        perfdata="Capacity=$mytest%;85;85;0;100"
+        if [[ $mytest -lt 85 ]] ; then
+                notices="$mytest% capacity remaining (<85%) "
                 ((warnings++))
         fi
 
         # APC UPS :: Battery Status
         mytest=`snmpget -r 3 -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.1.2.1.1.0 | awk '{print $NF}'`
-        if [[ $mytest -eq 2 ]] ; then
-                notices=$notices"Battery status OK "
-        elif [[ $mytest -eq 3 ]] ; then
+        if [[ $mytest -eq 3 ]] ; then
                 notices=$notices"Battery low "
                 ((warnings++))
-        else
+        elif [[ $mytest -ne 2 ]]; then
                 notices=$notices"Battery status unknown "
                 ((unknowns++))
         fi
 
         # APC UPS :: Replace Battery
         mytest=`snmpget -r 3 -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.1.2.2.4.0 | awk '{print $NF}'`
-        if [[ $mytest -eq 1 ]] ; then
-                notices=$notices"Battery health is good "
-        else
+        if [[ $mytest -ne 1 ]] ; then
                 notices=$notices"Battery needs to be replaced "
                 ((criticals++))
         fi
 
     if [[ $criticals -gt 0 ]] ; then
-        echo "Critical - "$notices"Please see http://$2/ for more information."
+        echo "Critical - "$notices"Please see http://$2/ for more information|$perfdata"
         exit 2
     elif [[ $warnings -gt 0 ]] ; then
-        echo "Warning - "$notices"Please see http://$2/ for more information."
+        echo "Warning - "$notices"Please see http://$2/ for more information|$perfdata"
         exit 1
     elif [[ $unknowns -gt 0 ]] ; then
-        echo "Unknown - "$notices"Please see http://$2/ for more information."
+        echo "Unknown - "$notices"Please see http://$2/ for more information|$perfdata"
         exit 3
     else
-        echo "OK - $notices""Please see http://$2/ for more information."
+        echo "OK|$perfdata"
         exit 0
     fi
 
@@ -105,12 +103,57 @@ elif [ $3 = 'load' ] ; then
 
         #APC UPS :: Advanced Output Load
         mytest=`snmpget -r 3 -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.1.4.2.3.0 | awk '{print $NF}'`
+        perfdata="Load=$mytest%;50;50;0;100"
         if [[ $mytest -lt 50 ]] ; then
-                echo "OK - Load is $mytest% which is less than 50% Please see http://$2/ for more information."
+                echo "OK - Load is $mytest% (<50%)|$perfdata"
                 exit 0
         else
-                echo "Warning - Load is $mytest% which is greater than 50% Please see http://$2/ for more information."
+                echo "Warning - Load is $mytest% (>50%) See http://$2/|$perfdata"
                 exit 1
+        fi
+elif [ $3 = 'temperature' ]; then
+        if [[ $# -ne 5 ]]; then
+                print_usage
+                exit 3
+        fi
+        if [[ $4 -gt $5 ]]; then
+                echo "The warning threshold must be >= the critical threshold."
+                exit 3
+        fi
+
+        mytest=$(snmpwalk -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.10.2.3.2.1.4 | wc -l)
+        if [[ $mytest -lt 1 ]]; then
+                echo "Unknown: No temperature sensors detected"
+                exit 3
+        fi
+
+
+        warnings=0
+        criticals=0
+        perfdata=""
+        while read rawdata; do
+                sensorindex=$(echo $rawdata | awk -F . '{print $NF}' | awk '{print $1}')
+                sensorname=$(snmpget -r 3 -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.10.2.3.2.1.2.$sensorindex | awk -F \" '{print $2}')
+                temperature=$(echo $rawdata | awk '{print $NF}')
+                perfdata="$sensorname=${temperature};$4;$5;0;113"
+                if [[ $temperature -gt $5 ]]; then
+                        ((criticals++))
+                        notices="$sensorname reads ${temperature}F $notices"
+                elif [[ $temperature -gt $4 ]]; then
+                        ((warnings++))
+                        notices="$sensorname reads ${temperature}F $notices"
+                fi
+        done < <(snmpwalk -v 1 -c $1 $2 1.3.6.1.4.1.318.1.1.10.2.3.2.1.4)
+
+        if [[ $criticals -gt 0 ]]; then
+                echo "Critical: $notices|$perfdata"
+                exit 2
+        elif [[ $warnings -gt 0 ]]; then
+                echo "Warning: $notices|$perfdata"
+                exit 1
+        else
+                echo "OK|$perfdata"
+                exit 0
         fi
 else
     print_usage
